0257e78e86d6d133a01b8ea2a3dac0278b2633f5
arashzam <arashzamzami2012@yahoo.com>
2019-08-10 02:46:20 -0700

    insert files

---
+++ includes/class-wp_clean_plug-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/arashzam
+ * @since      1.0.0
+ *
+ * @package    Wp_clean_plug
+ * @subpackage Wp_clean_plug/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_clean_plug
+ * @subpackage Wp_clean_plug/includes
+ * @author     arash <arashzamzami2012@yahoo.com>
+ */
+class Wp_clean_plug_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
